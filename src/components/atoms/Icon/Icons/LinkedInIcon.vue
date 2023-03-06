1621b9872307d2e132feaadba2943f40f0428054
Dmitriy <johndoe@example.com>
2023-03-06 15:26:45 +0200

    added icons

---
+++ src/components/atoms/Icon/Icons/LinkedInIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M10.96 22.5598C4.99295 22.5598 0.160034 17.7269 0.160034 11.7598C0.160034 5.7927 4.99295 0.959778 10.96 0.959778C16.9271 0.959778 21.76 5.7927 21.76 11.7598C21.76 17.7269 16.9271 22.5598 10.96 22.5598Z"
+            fill="#2D9CDB" />
+    </svg>
+</template>
+
+<script>
+
+export default {
+    name: 'LinkedInIcon'
+
+}
+</script>
--- src/components/atoms/Icon/choices.js
+++ src/components/atoms/Icon/choices.js
@@ -1,7 +1,15 @@
 import gmail from './Icons/GmailIcon.vue'
+import linkedin from './Icons/LinkedInIcon.vue'
+import phone from './Icons/PhoneIcon.vue'
+import skype from './Icons/SkypeIcon.vue'
+import telegram from './Icons/TelegramIcon.vue'
 
 const choices = {
-gmail
+gmail,
+linkedin,
+phone,
+skype,
+telegram
 };
 
 export default choices;
